99ecb1ea55f00f91b8598aa20152f2d702990b34
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2018-08-19 18:31:44 +0900

    Add interactivity (https://flutter.io/tutorials/interactive/): Step 2: Subclass StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -114,3 +114,8 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
     );
   }
 }
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
